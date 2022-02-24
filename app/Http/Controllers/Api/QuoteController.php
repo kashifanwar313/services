41010c0c3d8ed48060239634671150725596d8e7
kashifanwar <kashifanwar2015@gmail.com>
2022-02-24 16:47:50 +0500

    scheduled status fixed

--- app/Http/Controllers/Api/QuoteController.php
+++ app/Http/Controllers/Api/QuoteController.php
@@ -52,20 +52,33 @@ class QuoteController extends Controller
         $quote = Quote::where('hash_id', request()->quoteId)->first();
         if($quote)
         {
-            $update_quote = $quote->update([
-                'plans_id' => json_encode(request()->selectedPlans),
-                'signature' => request()->signature,
-                'total_estimated_time' => request()->totalTime,
-                'total_amount' => request()->totalPrice
-            ]);
-            if($update_quote){
+            if($quote->quote_status==1){
+                $update_quote = $quote->update([
+                    'plans_id' => json_encode(request()->selectedPlans),
+                    'signature' => request()->signature,
+                    'total_estimated_time' => request()->totalTime,
+                    'total_amount' => request()->totalPrice,
+                    'quote_status' => 3
+                ]);
+                if($update_quote){
+                    return response()->json([
+                        'success' => 'Quoted Successfully With Hash ID: '. request()->quoteId,
+                        'email' => json_decode($quote->contact)->email,
+                        'quoteId' => $quote->hash_id,
+                        'status' => 200,
+                        'quote_status' => $quote->quote_status
+                    ]);
+                }
+            } else {
                 return response()->json([
-                    'success' => 'Quoted Successfully With Hash ID: '. request()->quoteId,
+                    'success' => 'Quoted Already Planned With Hash ID: '. request()->quoteId,
                     'email' => json_decode($quote->contact)->email,
                     'quoteId' => $quote->hash_id,
-                    'status' => 200
+                    'status' => 200,
+                    'quote_status' => $quote->quote_status
                 ]);
             }
+
         }
         else
         {
@@ -83,7 +96,8 @@ class QuoteController extends Controller
             return response()->json([
                 'price_sheets' => $quote,
                 'quote_id' => $quote->hash_id,
-                'email' => json_decode($quote->contact)->email
+                'email' => json_decode($quote->contact)->email,
+                'quote_status' => $quote->quote_status,
             ]);
         }
         else{
